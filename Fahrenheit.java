4195360b35953d913e0a779d0795cea70a8d8b6c
Kuti-Ra <111650206+Kuti-Ra@users.noreply.github.com>
2022-09-19 15:27:41 -0400

    Add files via upload

---
+++ Fahrenheit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fahrenheit {
+
+ public static void main(String[]args) {
+  
+  Scanner input =new Scanner (System.in);
+  
+  
+   System.out.println("********************************************");
+  System.out.print("Enter temperature in Centigrade ");
+ int tempcel =input.nextInt();
+   System.out.println("********************************************");
+ int tempfar = ((tempcel*9)/5)+32;
+
+    System.out.println("Temperature in Fahrenheit is: "+tempfar);
+   }
+  }
